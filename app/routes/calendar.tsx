2955c92098a0412824ed0462131241e1863247e1
Luke Melong <lukemelong@protonmail.com>
2023-02-03 15:45:08 -0330

    Added calendar with actions for changing month

--- app/routes/calendar.tsx
+++ app/routes/calendar.tsx
@@ -1,9 +1,10 @@
 import styles from "~/styles/calendar/calendar.css"
 import { useState } from "react"
-import { getDaysInMonth, changeCurrentMonth } from "~/utils/utils"
+import { getDaysInMonth, changeCurrentDate, getFirstDayOfMonth, getMonthName } from "~/utils/utils"
 
 
 interface CalendarDateData {
+    key: string,
     day: number
 }
 
@@ -26,29 +27,43 @@ const CalendarDate = ({day}: CalendarDateData) => {
 export default function Calendar() {
 
     const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
     const daysInMonth: number = getDaysInMonth(currentMonth)
-
-    console.log(daysInMonth)
-
+    const firstDayOfMonth = getFirstDayOfMonth(currentMonth, currentYear)
     const dayElems: Array<any> = []
-    for(let i = 1; i <= daysInMonth; i++){
-        dayElems.push(<CalendarDate day={i}/>)
+
+    for(let i = 0; i < daysInMonth + firstDayOfMonth; i++){
+        if(i < firstDayOfMonth) {
+            dayElems.push(<div key={`${Date.now()}${i}`}></div>)
+            continue
+        }
+        dayElems.push(<CalendarDate key={`${new Date(currentMonth, currentYear, i).getTime()}`} day={i - firstDayOfMonth + 1}/>)
     }
 
-    console.log(dayElems)
+    const onChangeMonth = (add: boolean): void => {
+        const { month, year } = changeCurrentDate(currentMonth, currentYear, add)
+
+        setCurrentMonth(month)
+        setCurrentYear(year)
+    }
     return (
         <div>
-            <button onClick={() => setCurrentMonth(changeCurrentMonth(currentMonth, false))}>Previous Month</button>
-            <button onClick={() => setCurrentMonth(changeCurrentMonth(currentMonth, true))}>Next Month</button>
+            <div className="dateInformationContainer">
+                <p>{`${getMonthName(currentMonth)}, ${currentYear}`}</p>
+            </div>
+            <div className="dateChangeContianer">
+                <button onClick={() => onChangeMonth(false)}>Previous Month</button>
+                <button onClick={() => onChangeMonth(true)}>Next Month</button>
+            </div>
             <div className="calendarContainer">
                 <>
-                    <div>Sunday</div>
-                    <div>Monday</div>
-                    <div>Tuesday</div>
-                    <div>Wednesday</div>
-                    <div>Thursday</div>
-                    <div>Friday</div>
-                    <div>Saturday</div>
+                    <div key={'sundayDate'}>Sunday</div>
+                    <div key={'mondayDate'}>Monday</div>
+                    <div key={'tuesdayDate'}>Tuesday</div>
+                    <div key={'wednesdayDate'}>Wednesday</div>
+                    <div key={'thursdayDate'}>Thursday</div>
+                    <div key={'fridayDate'}>Friday</div>
+                    <div key={'saturdayDate'}>Saturday</div>
                     {
                         dayElems.map((elem) => elem)
                     }
@@ -57,3 +72,4 @@ export default function Calendar() {
         </div>
     )
 }
+
--- app/utils/utils.tsx
+++ app/utils/utils.tsx
@@ -7,42 +7,64 @@ const isLeapYear = (): boolean => {
 export const getDaysInMonth = (month: number): number => {
     let numOfDays: number = 0;
     switch(month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            numOfDays = 31
-            break
+        case 0:
+        case 2:
         case 4:
         case 6:
+        case 7:
         case 9:
         case 11:
+            numOfDays = 31
+            break
+        case 3:
+        case 5:
+        case 8:
+        case 10:
             numOfDays = 30
             break
-        case 2:
+        case 1:
             numOfDays = isLeapYear() ? 29 : 28
             break
     }
     return numOfDays
 }
 
-export const changeCurrentMonth = (currentMonth: number, addMonth: boolean): number => {
-    let nextMonth: number = 0;
+interface MonthAndYear {
+    month: number,
+    year: number
+}
+
+export const changeCurrentDate = (currentMonth: number, currentYear: number, addMonth: boolean): MonthAndYear => {
+    let nextMonth: number = -1
+    let nextYear: number = currentYear
+
     if(addMonth) {
-        if(currentMonth === 11) nextMonth = 0
+        if(currentMonth === 11) {
+            nextMonth = 0
+            nextYear++
+        }
         else nextMonth = currentMonth + 1
     }
     else {
-        if(currentMonth === 0) nextMonth = 11
+        if(currentMonth === 0) {
+            nextMonth = 11
+            nextYear--
+        }
         else nextMonth = currentMonth - 1
     }
 
-    return nextMonth
+    return {
+        month: nextMonth,
+        year: nextYear
+    }
 }
 
-export const getFirstDayOfMonth = () => {
-    return
+export const getFirstDayOfMonth = (month: number, year: number): number => {
+    return new Date(year, month, 1).getDay()
+}
+
+export const getMonthName = (month: number): string => {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    return monthNames[month]
 }
